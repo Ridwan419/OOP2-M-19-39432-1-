6851cc0b0efe47206c07e9bc8df5d86ca095fbb8
Md Ridwan Islam <19-39432-1@student.aiub.edu>
2021-02-09 02:51:57 +0600

    Console Application1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = 100;
+            for (i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("All the even numbers are: " + i);
+                }
+            }
+        }
+    }
+}
