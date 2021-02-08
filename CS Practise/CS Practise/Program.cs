35ed6dbd7097375782d3c0003b4bd410fa99a278
Md Ridwan Islam <19-39432-1@student.aiub.edu>
2021-02-09 03:31:59 +0600

    CS Practice

---
+++ CS Practise/CS Practise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student obj1 = new Student("Ridwan", "39432", "CSE", 3.11f);
+            obj1.showInfo();
+            Triangle obj2 = new Triangle(10, 20, 10);
+            obj2.ShowInfo();
+            obj2.TestTriangle();
+            Course obj3 = new Course("OOP2", "345432", 3);
+            obj3.ShowInfo1();
+            Account obj4 = new Account("Ridwan", "6565", 400);
+            obj4.Deposit(400);
+            obj4.Withdraw(400);
+            
+        }
+    }
+}
